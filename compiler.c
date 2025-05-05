307615ab7038583a398b9eb52b1a8c73e5f3fe07
jesusd63 <jusus.diazb2003@gmail.com>
2025-05-04 20:00:17 -0600

    Codigo comentado

--- compiler.c
+++ compiler.c
@@ -26,6 +26,7 @@ int es_espacio(char c) {
     return c == ' ' || c == '\n' || c == '\t' || c == '\r';
 }
 
+// Funcion para determinar si un caracter es un delimitador
 int es_delimitador(char c) {
     return c == ';' || c == ',' || c == '.' || c == ':' || c == '=' || c == '*' || c == '&' || c == '<' || c == '>';
 }
@@ -46,14 +47,15 @@ const char* simbolos[] = {
     ";", "*", ",", "&", "<", ">", ".", ":", "="
 };
 
-// Definicion de la cantidad de palabras clave
+// Marcar el token de identificadores y strings
 int identifier_id = 1 + sizeof(separadores)/sizeof(separadores[0]) + sizeof(palabras_clave)/sizeof(palabras_clave[0]) + sizeof(simbolos)/sizeof(simbolos[0]);
 int string_id;
 
+// Contadores de tokens
 int identifier_count = 0;
-int number_count = 0;
 int string_count = 0;
 
+// Funcion que determina si un estado es un estado aceptador
 int Accept(int estado){
     if(estado == 1 || estado == 2 || estado == 3 || estado == 4 || estado == 5 || estado == 7 || estado == 9){
         return 1; // Aceptar el token
@@ -61,25 +63,30 @@ int Accept(int estado){
     return 0; // No aceptar el token
 }
 
+// Funcion que determina si un estado es un estado de error
 int Error(int estado){
     return estado == 10; // Estado de error
 }
 
+// Tabla de transiciones del DFA
+// Estado 0: INICIO
+// Estado 10: ERROR
+
 int TransitionTable[10][10] = {
     [0] = {6, 6, 5, 5, 1, 2, 3, 4, 8, 10}, // Estado de inicio
-    [1] = {0}, // Estado inicial
-    [2] = {0}, // Estado de identificador
-    [3] = {0}, // Estado de numero
-    [4] = {0}, // Estado de cadena
-    [5] = {0}, // Estado de separador
-    [6] = {6, 6, 6, 7, 7, 7, 7, 7, 7, 10},
-    [7] = {0}, // Estado de simbolo
-    [8] = {8, 8, 8, 8, 8, 8, 8, 8, 9, 8}, // Estado de palabra clave
-    [9] = {0} // Estado de fin
+    [1] = {0}, // Estado de (
+    [2] = {0}, // Estado de )
+    [3] = {0}, // Estado de {
+    [4] = {0}, // Estado de }
+    [5] = {0}, // Estado de aceptacion de otros caracteres
+    [6] = {6, 6, 6, 7, 7, 7, 7, 7, 7, 10}, // Estado de identificador
+    [7] = {0}, // Estado de aceptacion de identificador
+    [8] = {8, 8, 8, 8, 8, 8, 8, 8, 9, 8}, // Estado de string
+    [9] = {0} // Estado de aceptacion de string
 }; 
 
+// Funcion que determina la columna de la tabla de transiciones segun el caracter
 int char_to_col(char c) {
-    //printf("Caracter a cambiar: %c\n", c);
     if (es_letra(c)){
         return 0; // Columna para letras
     } 
@@ -108,22 +115,20 @@ int char_to_col(char c) {
         return 8; // Columna para comillas
     }
     else {
-        return 9; // Columna para espacio en blanco
+        return 9; // Columna para caracteres no reconocidos
     }
 
-    return -1; // Caracter no reconocido
+    return -1;
 }
 
+// Funcion que determina el nuevo estado del DFA segun el estado actual y el caracter
 int T(int estado, char c) {
     int col = char_to_col(c);
-    // printf("Columna: %d\n", col);
     return TransitionTable[estado][col]; // Retorna el nuevo estado
 }
 
+// Funcion que determina si un estado puede seguir aceptando caracteres
 int Advance(int estado, char c) {
-    // Si estamos procesando una cadena, siempre avanzamos
-    if (estado == 4 || estado == 8 || estado == 9) return 1;
-    
     // Si estamos en un estado aceptador, no avanzar (porque ya tenemos un token completo)
     if (Accept(estado)) return 0;
 
@@ -131,41 +136,58 @@ int Advance(int estado, char c) {
     return 1;
 }
 
+// Funcion que imprime el token
 void record_token(char* buffer, int index, int estado) {
-    buffer[index] = '\0'; // Terminar el string
-    if (estado == 1 ) { // 
-        printf("1, %s\n", buffer);
-    } else if (estado == 2) { // 
-        printf("2, %s\n", buffer);
-    } else if (estado == 3) { // 
-        printf("3, %s\n", buffer);
-    } else if (estado == 4) { // 
-        printf("4, %s\n", buffer);
-    } else if (estado == 5) { // 
+    // Terminar el string
+    buffer[index] = '\0';
+
+    // Estados aceptadores de parentesis y llaves
+    for(int i = 1; i < 5; i++){
+        if(estado == i){
+            // Imprimir el token
+            printf("%d, %s\n", i, buffer);
+        }
+    }
+
+    // Estado aceptador de delimitadores y otros caracteres
+    if(estado == 5) {
+        // Si el caracter es un espacio, ignorarlo 
         if(es_espacio(buffer[0])){
             return;
         }
+        // Buscar si el caracter es un simbolo reservado
         for(int i = 0; i < sizeof(simbolos)/sizeof(simbolos[0]); i++){
             if(strcmp(buffer, simbolos[i]) == 0){
                 printf("%d, %s\n",i+sizeof(palabras_clave)/sizeof(palabras_clave[0])+sizeof(separadores)/sizeof(separadores[0])+1, buffer);
                 return;
             }
         }
+        // Sumar a la cuenta de delimitadores
         identifier_count++;
-        printf("%d, %d, %s\n" , identifier_id, identifier_count, buffer); // Imprimir el token
+        // Imprimir el token
+        printf("%d, %d, %s\n" , identifier_id, identifier_count, buffer);
+    
+    // Estado aceptador de identificadores
     } else if (estado == 7) { // 
+        // Buscar si la palabra es una palabra clave
         for(int i = 0; i < sizeof(palabras_clave)/sizeof(palabras_clave[0]); i++){
             if(strcmp(buffer, palabras_clave[i]) == 0){
-                // printf("PALABRA CLAVE\n");
+                // Imprimir el token
                 printf("%d, %s\n",i+sizeof(separadores)/sizeof(separadores[0])+1, buffer);
                 return;
             }
         }
+        // Sumar a la cuenta de identificadores
         identifier_count++;
-        printf("%d, %d, %s\n" , identifier_id, identifier_count, buffer); // Imprimir el token
-    } else if (estado == 9){
+        // Imprimir el token
+        printf("%d, %d, %s\n" , identifier_id, identifier_count, buffer);
+    }
+    // Estado aceptador de strings 
+    else if (estado == 9){
+        // Sumar a la cuenta de strings
         string_count++;
-        printf("%d, %d, %s\n" , string_id, string_count, buffer); // Imprimir el token
+        // Imprimir el token
+        printf("%d, %d, %s\n" , string_id, string_count, buffer);
     }
 }
 
@@ -191,45 +213,51 @@ int main(int argc, char* argv[]) {
     char c, buffer[MAX_TOKEN];
     int index = 0;
 
-    // Inicializar el estado del DFA en INICIO
+    // Leer el primer caracter del archivo
     c = fgetc(archivo);
     while (c != EOF) {
+        // Inicializar el estado del DFA en INICIO
         int estado = 0;
-        index = 0; // Reiniciar el buffer
+        // Reiniciar el buffer
+        index = 0;
         while(!Accept(estado) && !Error(estado)){
-            // printf("Estado inicial: %d, Caracter: %c\n", estado, c);
+            // Determinar el nuevo estado del DFA
             estado = T(estado, c);
-            // printf("Estado: %d, Caracter: %c\n", estado, c);
-            buffer[index++] = c; // Guardar el caracter en el buffer
+            // Guardar el caracter en el buffer
+            buffer[index++] = c;
+
             if(Advance(estado, c)){
-                c = fgetc(archivo); // Leer el siguiente caracter
+                // Leer el siguiente caracter
+                c = fgetc(archivo);
             }
-            // else{
-            //     if(estado != 0){
-            //         buffer[index-1] = '\0'; // Terminar el string
-            //         ungetc(c, archivo); // Devolver el caracter al flujo de entrada
-            //     }
-            // }
         }
+        // El estado es aceptador o de error
         if(Accept(estado)){
+            // Si el caracter es un espacio, ignorarlo
             if(es_espacio(buffer[0])){
-                c = fgetc(archivo); // Leer el siguiente caracter
+                // Leer el siguiente caracter
+                c = fgetc(archivo);
                 continue;
             }
+            // Si el estado es 7 (aceptor de identificador)
             if(estado == 7){
+                // Ignorar el ultimo caracter
                 index--; 
-                ungetc(c, archivo); // Devolver el caracter al flujo de entrada
+                // Devolver el caracter al flujo de entrada
+                ungetc(c, archivo);
             }
+            // Imprimir el token
             record_token(buffer, index, estado);
-            // printf("Estado aceptador: %d, Caracter: %c\n", estado, c);
-            
-            c = fgetc(archivo); // Leer el siguiente caracter
-        } else{
+            // Leer el siguiente caracter
+            c = fgetc(archivo);
+        } else{ // Si el estado es de error
             printf("Error: token no reconocido.\n");
+            // Terminar el programa
             break;
         }
     }
 
+    // Cerrar el archivo
     fclose(archivo);
     return 0;
 }
